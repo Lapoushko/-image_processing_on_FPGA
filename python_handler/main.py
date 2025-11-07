e980013e7523b5fd30303f9b62661c24c3896302
zahZah <nikitivnae@gmail.com>
2025-11-07 10:09:32 +0500

    Поправил для сборки

--- python_handler/main.py
+++ python_handler/main.py
@@ -3,8 +3,8 @@ import processor
 from modification.filter.impl.Color import FilterColors, AvailableColors
 from modification.filter.impl.Emboss import Emboss
 from modification.transpose.impl.Rotate import Rotate
-from python_handler.mapper.coe_to_image import coe_to_image
-from python_handler.mapper.image_to_coe import image_to_coe
+from mapper.coe_to_image import coe_to_image
+from mapper.image_to_coe import image_to_coe
 
 coe_image = image_to_coe("06.jpg", "output.coe", mode="RGB")
 image_universal = coe_to_image("output.coe", "restored.png")
