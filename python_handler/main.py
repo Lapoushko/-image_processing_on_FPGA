66d617f1aa8407f059d6400b0a85102b9f9c7f8e
zahZah <nikitivnae@gmail.com>
2025-11-07 10:57:48 +0500

    Друг Вадима и Лена

--- python_handler/main.py
+++ python_handler/main.py
@@ -9,16 +9,39 @@ from mapper.image_to_coe import image_to_coe
 coe_image = image_to_coe("06.jpg", "output.coe", mode="RGB")
 image_universal = coe_to_image("output.coe", "restored.png")
 
+coe_image_LENA = image_to_coe("7.jpg", "LENA.coe", mode="RGB")
+image_universal_LENA = coe_to_image("LENA.coe", "restored_LENA.png")
 
-image = image_loader.get_image("restored.png")
-processor = processor.Processor(image)
+image_drug_vadima = image_loader.get_image("restored.png")
+image_LENA = image_loader.get_image("restored_LENA.png")
 
-processor.add_filter(Emboss())
-processor.add_filter(FilterColors(processor.get_last_image(), AvailableColors.GREEN).get_color())
+processor_LENA = processor.Processor(image_LENA)
 
-processor.transposing(Rotate(45))
+processor_LENA.add_filter(Emboss())
 
-processor.undo()
-processor.redo()
+processor_LENA.add_filter(FilterColors(processor_LENA.get_last_image(), AvailableColors.GREEN).get_color())
+
+processor_LENA.transposing(Rotate(45))
+
+processor_LENA.undo()
+
+processor_LENA.redo()
+
+processor_LENA.show_image()
+
+
+processor_drug_vadima = processor.Processor(image_drug_vadima)
+
+processor_drug_vadima.add_filter(Emboss())
+
+processor_drug_vadima.add_filter(FilterColors(processor_drug_vadima.get_last_image(), AvailableColors.GREEN).get_color())
+
+processor_drug_vadima.transposing(Rotate(45))
+
+processor_drug_vadima.undo()
+
+processor_drug_vadima.redo()
+
+processor_drug_vadima.show_image()
 
 processor.show_image()
